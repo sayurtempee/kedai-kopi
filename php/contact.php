b2ec457e07166fc7e1e7af6acd34f53717d07f64
kazuo <kazuo@kazuo-mii>
2025-01-23 20:53:22 +0700

    modified

--- php/contact.php
+++ php/contact.php
@@ -18,7 +18,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $sql = "INSERT INTO contact (nama, email, no_hp) VALUES ('$nama', '$email', '$no_hp')";
 
     if ($conn->query($sql) === TRUE) {
-        echo "<script>alert('Data Anda Berhasil Terkirim!!'); window.location.href = '../index.php';</script>";
+        header("Location: ../index.php");
     } else {
         echo "Error: " . $sql . "<br>" . $conn->error;
     }
